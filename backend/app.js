f3b92d519d82749340787d8a8de33e518d02bbc6
bit <pipecarnal123@gmail.com>
2024-10-08 23:51:13 +0000

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Docker! This is a sample change! This is version 2);
+});
+
+app.get('/secret', (req, res) => {
+    res.send('You found a new secret!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
